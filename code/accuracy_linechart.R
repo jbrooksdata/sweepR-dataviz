ecabb67defb4d07019f89a924c6306ac0e31d0d5
Jeremy Brooks <jeremydanielbrooks@yahoo.com>
2022-03-19 23:31:49 -0500

    label changes

--- code/accuracy_linechart.R
+++ code/accuracy_linechart.R
@@ -7,7 +7,7 @@ library(ggthemes)
 mens_games <- load_team_games("mens") %>%
   filter(team %in% c("GBR","USA","CAN","SWE")) %>%
   summarise(team,opp,team_game_number,stage,throws,throw_accuracy) %>%
-  group_by(team) %>% 
+  group_by(team) %>%
   mutate(rec = 1) %>%
   mutate(rollavg = cumsum(throw_accuracy)/cumsum(rec)) %>%
   select(-rec)
@@ -17,13 +17,13 @@ mens_games %>%
   ggplot(aes(x = team_game_number, y = rollavg, color = team)) +
   geom_line(size = 1.25) + # increase line weight
   ylim(80,90) + # y axis range
-  labs(title = "Men's Finalists - Overall Throwing Accuracy",
-       subtitle = "How does accuracy change as the tournament progresses?",
+  labs(title = "Running Throw Accuracy for the Men's Playoff Teams",
+       subtitle = "How did the medal candidates compare in terms of overall throw accuracy?",
        caption = "@jbrooksdata | data: sweepR",
        x = "Game Number",
-       y = "Running Throw Accuracy %",
+       y = "Throw Accuracy %",
        color = "Nation:") +
-  theme_fivethirtyeight() + 
+  theme_fivethirtyeight() +
   theme(axis.title = element_text()) + # show x and y labels
   scale_color_manual(values = c("#CD7F32", #line colors: gold, silver, bronze, black
                                 "#C0C0C0",
